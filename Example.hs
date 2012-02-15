1416f4c0ff82999102d487cc3bb61692ac18bcb6
Maxime Henrion <mhenrion@gmail.com>
2012-02-15 01:03:27 +0100

    Various improvements and showcase new features.
    - We are now able to gather dictionary keys such as "created by" using a
      translation function to map it to the "createdBy" accessor.
    - Cleanup the formatting of the output and display more information.
    - Gracefully handle malformed torrent files.

--- Example.hs
+++ Example.hs
@@ -1,19 +1,21 @@
 {-# LANGUAGE DeriveGeneric, OverloadedStrings #-}
 module Main where
 
-import Control.Applicative
 import Control.Monad
 import Data.ByteString.Char8 (ByteString)
 import qualified Data.ByteString.Char8 as B
 import GHC.Generics
 import System.Environment
+import System.Exit
 import System.FilePath
 import Data.Bencode
 import Data.Bencode.Generic
 
 data Torrent =
-  Torrent { announce :: ByteString
-          , info     :: TorrentInfo
+  Torrent { announce  :: ByteString
+          , comment   :: ByteString
+          , createdBy :: ByteString
+          , info      :: TorrentInfo
           } deriving (Show, Generic)
 
 data TorrentInfo =
@@ -25,16 +27,25 @@ data File =
        , length :: Integer
        } deriving (Show, Generic)
 
-instance FromBencode Torrent
+instance FromBencode Torrent where
+  accessorMap = AM (\f -> if f == "createdBy" then "created by" else f)
+
 instance FromBencode TorrentInfo
 instance FromBencode File
 
 main :: IO ()
 main = do
-  (file:_)       <- getArgs
-  Just (bdata,_) <- bdecode <$> B.readFile file
-  let Just torrent = fromBencode bdata
-  forM_ (files (info torrent)) $ \(File pcs size) -> do
-    B.putStr (B.intercalate (B.singleton pathSeparator) pcs)
-    B.putStr " "
-    B.putStrLn (B.pack (show size))
+  (file:_) <- getArgs
+  content  <- B.readFile file
+  case bdecode_ content >>= fromBencode of
+    Nothing      ->
+      do B.putStrLn "Cannot parse torrent file"
+         exitFailure
+    Just torrent ->
+      do B.putStrLn $ "Torrent name:\t" `B.append` (comment torrent)
+         B.putStrLn $ "Announce URL:\t" `B.append` (announce torrent)
+         B.putStrLn $ "Created by:\t" `B.append` (createdBy torrent)
+         B.putStrLn ""
+         forM_ (files (info torrent)) $ \(File pcs size) -> do
+           B.putStrLn $ B.intercalate (B.singleton pathSeparator) pcs
+                          `B.append` B.pack (' ':show size)
